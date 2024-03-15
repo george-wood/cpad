818021319428b6c536309f59ebad6ff78d1b1f2a
George Wood <george.b.wood@googlemail.com>
2024-03-15 11:04:01 +0000

    Port force import to polars

--- import/force.R
+++ import/force.R
@@ -15,229 +15,226 @@ source <- function() {
 
 #' Define the schema
 #' @export
-get_schema <- function(reference, sheet) {
-
-  box::use(
-    arrow[schema, string, timestamp, date32],
-    cli[cli_abort]
+get_schema <- function() {
+  list(
+    # report
+    TRR_REPORT_ID = "character",
+    RD_NO = "character",
+    CR_NO_OBTAINED = "character",
+    SUBJECT_CB_NO = "character",
+    SUBJECT_IR_NO = "character",
+    EVENT_NO = "character",
+    BEAT = "character",
+    BLK = "character",
+    DIR = "character",
+    STN = "character",
+    LOC = "character",
+    DATETIME = "character",
+    DTE = "character",
+    TMEMIL = "character",
+    INDOOR_OR_OUTDOOR = "character",
+    LIGHTING_CONDITION = "character",
+    WEATHER_CONDITION = "character",
+    NOTIFY_OEMC = "character",
+    NOTIFY_DIST_SERGEANT = "character",
+    NOTIFY_OP_COMMAND = "character",
+    NOTIFY_DET_DIV = "character",
+    NUMBER_OF_WEAPONS_DISCHARGED = "character",
+    PARTY_FIRED_FIRST = "character",
+    POLAST = "character",
+    POFIRST = "character",
+    POMI = "character",
+    POGNDR = "character",
+    PORACE = "character",
+    POYRofBIRTH = "integer",
+    POAGE = "integer",
+    APPOINTED_DATE = "character",
+    UNITASSG = "character",
+    UNITDETAIL = "character",
+    ASSGNBEAT = "character",
+    RANK = "character",
+    DUTYSTATUS = "character",
+    POINJURED = "character",
+    MEMBER_IN_UNIFORM = "character",
+    CURRRANK = "character",
+    CURRUNIT = "character",
+    CURRBEATASSG = "character",
+    SUBGNDR = "character",
+    SUBRACE = "character",
+    SUBYEARDOB = "integer",
+    SUBAGE = "integer",
+    SUBJECT_ARMED = "character",
+    SUBJECT_INJURED = "character",
+    SUBJECT_ALLEGED_INJURY = "character",
+    TRR_CREATED = "character",
+    POLICYCOMPLIANCE = "character",
+    # action
+    PERSON = "character",
+    `Resistance Type` = "character",
+    ACTION = "character",
+    OTHERDESCR = "character"
   )
-
-  schema_A <- schema(
-    uid_force = string(),
-    rd = string(),
-    cr = string(),
-    cb = string(),
-    event = string(),
-    beat = string(),
-    block = string(),
-    street_direction = string(),
-    street = string(),
-    location = string(),
-    date = string(),
-    time = string(),
-    outdoor = string(),
-    lighting_condition = string(),
-    weather_condition = string(),
-    notify_oemc = string(),
-    notify_district_sergeant = string(),
-    notify_operational_command = string(),
-    notify_detective_division = string(),
-    weapons_discharged = double(),
-    party_fired_first = string(),
-    last_name = string(),
-    first_name = string(),
-    gender = string(),
-    race = string(),
-    age = double(),
-    appointed = string(),
-    assigned_unit = string(),
-    assigned_unit_detail = string(),
-    assigned_beat = string(),
-    rank = string(),
-    duty_status = string(),
-    officer_injured = string(),
-    in_uniform = string(),
-    civilian_gender = string(),
-    civilian_race = string(),
-    civilian_age = double(),
-    civilian_yob = double(),
-    civilian_armed = string(),
-    civilian_injured = string(),
-    civilian_alleged_injury = string()
-  )
-
-  schema_B <- schema(
-    uid_force = string(),
-    rd = string(),
-    cr = string(),
-    cb = string(),
-    ir = string(),
-    event = string(),
-    beat = string(),
-    block = string(),
-    street_direction = string(),
-    street = string(),
-    location = string(),
-    dt = string(),
-    outdoor = string(),
-    lighting_condition = string(),
-    weather_condition = string(),
-    notify_oemc = string(),
-    notify_district_sergeant = string(),
-    notify_operational_command = string(),
-    notify_detective_division = string(),
-    weapons_discharged = double(),
-    party_fired_first = string(),
-    last_name = string(),
-    first_name = string(),
-    middle_initial = string(),
-    gender = string(),
-    race = string(),
-    yob = double(),
-    appointed = string(),
-    assigned_unit = string(),
-    assigned_unit_detail = string(),
-    assigned_beat = string(),
-    rank = string(),
-    duty_status = string(),
-    officer_injured = string(),
-    in_uniform = string(),
-    current_rank = string(),
-    current_unit = string(),
-    current_beat_assigned = string(),
-    civilian_gender = string(),
-    civilian_race = string(),
-    civilian_yob = double(),
-    civilian_armed = string(),
-    civilian_injured = string(),
-    civilian_alleged_injury = string(),
-    report_created = timestamp(),
-    policy_compliance = string(),
-  )
-
-  schema_C <- schema(
-    uid_force = string(),
-    person = string(),
-    resistance_type = string(),
-    action = string(),
-    description = string()
-  )
-
-  schema_D <- schema(
-    uid_force = string(),
-    person = string(),
-    resistance_type = string(),
-    action = string()
-  )
-
-  schm <- list(
-    p046360_0 = schema_A,
-    p046360_2 = schema_C,
-    p456008_0 = schema_A,
-    p456008_2 = schema_C,
-    p461899_0 = schema_A,
-    p461899_2 = schema_C,
-    p583646_1 = schema_B,
-    p583646_2 = schema_D
-  )
-
-  if (missing(reference)) {
-    schm
-  } else {
-    if (missing(sheet)) {
-      cli_abort("Specify a sheet, e.g. `sheet = 0`")
-    }
-    schm[[paste(reference, sheet, sep = "_")]]
-  }
-
 }
 
+#' Alias for column names
+alias <- function(reference) {
+  list(
+    # report
+    uid_force = "TRR_REPORT_ID",
+    rd = "RD_NO",
+    cr = "CR_NO_OBTAINED",
+    cb = "SUBJECT_CB_NO",
+    ir = "SUBJECT_IR_NO",
+    event = "EVENT_NO",
+    beat = "BEAT",
+    block = "BLK",
+    street_direction = "DIR",
+    street = "STN",
+    location = "LOC",
+    dt = "DATETIME",
+    date = "DTE",
+    time = "TMEMIL",
+    outdoor = "INDOOR_OR_OUTDOOR",
+    lighting_condition = "LIGHTING_CONDITION",
+    weather_condition = "WEATHER_CONDITION",
+    notify_oemc = "NOTIFY_OEMC",
+    notify_district_sergeant = "NOTIFY_DIST_SERGEANT",
+    notify_operational_command = "NOTIFY_OP_COMMAND",
+    notify_detective_division = "NOTIFY_DET_DIV",
+    weapons_discharged = "NUMBER_OF_WEAPONS_DISCHARGED",
+    party_fired_first = "PARTY_FIRED_FIRST",
+    last_name = "POLAST",
+    first_name = "POFIRST",
+    middle_initial = "POMI",
+    gender = "POGNDR",
+    race = "PORACE",
+    yob = "POYRofBIRTH",
+    age = "POAGE",
+    appointed = "APPOINTED_DATE",
+    assigned_unit = "UNITASSG",
+    assigned_unit_detail = "UNITDETAIL",
+    assigned_beat = "ASSGNBEAT",
+    rank = "RANK",
+    duty_status = "DUTYSTATUS",
+    officer_injured = "POINJURED",
+    in_uniform = "MEMBER_IN_UNIFORM",
+    current_rank = "CURRRANK",
+    current_unit = "CURRUNIT",
+    current_beat = "CURRBEATASSG",
+    civilian_gender = "SUBGNDR",
+    civilian_race = "SUBRACE",
+    civilian_yob = "SUBYEARDOB",
+    civilian_age = "SUBAGE",
+    civilian_armed = "SUBJECT_ARMED",
+    civilian_injured = "SUBJECT_INJURED",
+    civilian_alleged_injury = "SUBJECT_ALLEGED_INJURY",
+    report_created = "TRR_CREATED",
+    policy_compliance = "POLICYCOMPLIANCE",
+    # action
+    person = "PERSON",
+    resistance_type = "Resistance Type",
+    action = "ACTION",
+    description = "OTHERDESCR"
+  )
+}
 
 #' Read the data, apply schema, and write dataset
 #' @export
 build <- function(report, action) {
+  box::use(polars[pl])
 
-  box::use(
-    arrow[read_csv_arrow],
-    clock[get_year],
-    dplyr[coalesce, distinct, if_else, left_join, mutate],
-    purrr[map, list_rbind],
-    tidyselect[starts_with],
-    proc/utility[get_reference, get_sheet, parse_dt, pad_time, polarize]
-  )
-
-  report <-
-    map(
+  pl$concat(
+    lapply(
       report,
       \(x)
-      read_csv_arrow(
-        file = x,
-        schema = get_schema(
-          reference = get_reference(x),
-          sheet = get_sheet(x)
+      pl$
+        scan_csv(
+          x,
+          dtypes = get_schema(),
+          try_parse_dates = FALSE
+        )$
+        rename(
+          intersect(alias(), pl$scan_csv(x)$columns)
+        )
+    ),
+    how = "diagonal"
+  )$
+    with_columns(
+      pl$
+        col(
+          c("notify_oemc",
+            "notify_district_sergeant",
+            "notify_operational_command",
+            "notify_detective_division",
+            "duty_status",
+            "officer_injured",
+            "in_uniform",
+            "civilian_armed",
+            "civilian_injured",
+            "civilian_alleged_injury")
+        )$
+        is_in(
+          c("Y", "True", "TRUE")
         ),
-        timestamp_parsers = c(
-          "%Y-%b-%d %H%M",
-          "%m/%d/%y %H%M",
-          "%Y-%b-%d %H%M",
-          "%Y-%m-%dT%H:%M:%S"
-        ),
-        skip = 1
-      )
-    ) |>
-    list_rbind() |>
-    mutate(
-      appointed = parse_dt(
-        appointed,
-        format = c("%Y-%b-%d",
-                   "%Y-%m-%d",
-                   "%Y/%m/%d")
+      # dates
+      pl$coalesce(
+        pl$col("appointed")$str$to_date(format = "%Y-%m-%d", strict = FALSE),
+        pl$col("appointed")$str$to_date(format = "%Y/%m/%d", strict = FALSE),
+        pl$col("appointed")$str$to_date(format = "%Y-%b-%d", strict = FALSE)
       ),
-      dt = parse_dt(
-        coalesce(dt, paste(date, pad_time(time))),
-        format = c("%Y-%m-%d %H%M",
-                   "%m/%d/%y %H%M",
-                   "%Y-%b-%d %H%M",
-                   "%Y/%m/%d %H%M")
+      pl$coalesce(
+        pl$
+          col("dt")$
+          str$to_datetime(
+            format = "%Y-%b-%d %H%M",
+            strict = FALSE
+          ),
+        pl$
+          concat_str(pl$col("time")$str$zfill(4), pl$col("date"))$
+          str$to_datetime(
+            format = "%H%M%Y-%m-%d",
+            strict = FALSE
+          ),
+        pl$
+          concat_str(pl$col("time")$str$zfill(4), pl$col("date"))$
+          str$to_datetime(
+            format = "%H%M%Y/%m/%d",
+            strict = FALSE
+          )
       ),
-      across(
-        where(
-          function(x)
-            all(x[!is.na(x)] %in% c("True", "TRUE", "False", "FALSE"))
-        ),
-        function(z) z %in% c("True", "TRUE")
-      ),
-      yob_lower = if_else(
-        is.na(yob),
-        get_year(appointed) - age - 1,
-        yob
-      ),
-      .keep = "unused"
-    ) |>
-    distinct()
-
-  action <-
-    map(
-      action,
-      \(x)
-      read_csv_arrow(
-        file = x,
-        schema = get_schema(
-          reference = get_reference(x),
-          sheet = get_sheet(x)
-        ),
-        skip = 1
-      )
-    ) |>
-    list_rbind() |>
-    distinct()
-
-  left_join(
-    report,
-    action,
-    by = "uid_force"
-  ) |>
-    polarize()
-
+      pl$col("report_created")$
+        str$to_datetime(format = "%Y-%m-%dT%H:%M:%S", strict = FALSE)
+    )$
+    with_columns(
+      pl$col("dt")$dt$year()$
+        sub(pl$col("age"))$
+        sub(1)$
+        alias("yob_lower")
+    )$
+    drop(
+      c("date", "time")
+    )$
+    unique()$
+    join(
+      other =
+        pl$concat(
+          lapply(
+            action,
+            \(x)
+            pl$
+              scan_csv(
+                x,
+                dtypes = get_schema(),
+                try_parse_dates = FALSE
+              )$
+              rename(
+                intersect(alias(), pl$scan_csv(x)$columns)
+              )
+          ),
+          how = "diagonal"
+        )$
+        unique(),
+      on = "uid_force",
+      how = "left"
+    )
 }
-
